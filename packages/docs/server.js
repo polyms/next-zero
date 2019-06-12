7b75279bc3f882097b88b3a219c5e19a48a3b421
vHades <anhvu.nd.0199@gmail.com>
2019-06-12 08:36:03 +0700

    fix

--- packages/docs/server.js
+++ packages/docs/server.js
@@ -1,1 +1,12 @@
-module.exports = require('./.build/server');
+/* eslint-disable import/no-extraneous-dependencies */
+import apm from 'elastic-apm-node';
+import server from '../next-zero/src/server';
+
+apm.start({
+  // Ignore requests to certain URLs from being instrumented
+  ignoreUrls: ['/', '/spec'],
+  // Only activate the agent if it's running in production and apm server is set
+  active: process.env.NODE_ENV === 'production' && !!process.env.ELASTIC_APM_SERVER_URL,
+});
+
+server({ nextConfig: {} });
