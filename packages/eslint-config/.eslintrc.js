534101785c25a36e390a441085a15087a6fd99e4
vHades <anhvu.nd.0199@gmail.com>
2019-09-19 21:14:18 +0700

    fix eslint

--- packages/eslint-config/.eslintrc.js
+++ packages/eslint-config/.eslintrc.js
@@ -1,4 +1,4 @@
-const restrictedGlobals = require('eslint-restricted-globals');
+const restrictedGlobals = require("eslint-restricted-globals");
 
 const OFF = 0;
 const WARNING = 1;
@@ -6,24 +6,23 @@ const ERROR = 2;
 
 module.exports = {
   extends: [
-    'airbnb',
-    'plugin:flowtype/recommended',
-    'plugin:jest/recommended',
-    'prettier',
-    'prettier/flowtype',
-    'prettier/react',
+    "airbnb",
+    "plugin:flowtype/recommended",
+    "plugin:jest/recommended",
+    "prettier",
+    "prettier/flowtype",
+    "prettier/react"
   ],
   env: {
     browser: true,
     es6: true,
-    node: true,
+    node: true
   },
-  parser: 'babel-eslint',
-  plugins: ['babel', 'flowtype', 'jest', 'prettier'],
+  parser: "babel-eslint",
+  plugins: ["babel", "flowtype", "jest", "prettier"],
   rules: {
-    'prettier/prettier': ERROR,
-    'class-methods-use-this': OFF,
-    'flowtype/no-weak-types': WARNING,
+    "class-methods-use-this": OFF,
+    "flowtype/no-weak-types": WARNING,
     // 'flowtype/require-parameter-type': OFF,
     // 'flowtype/require-return-type': [OFF, 'always', { annotateUndefined: 'never' }],
     // 'flowtype/require-valid-file-annotation': ERROR,
@@ -32,27 +31,28 @@ module.exports = {
     // 'import/no-unresolved': ERROR,
     'import/prefer-default-export': OFF,
     // 'new-cap': OFF,
-    'no-class-assign': OFF,
+    // 'no-class-assign': OFF,
     // 'no-plusplus': OFF,
-    'no-restricted-globals': ['error'].concat(restrictedGlobals),
+    "no-restricted-globals": ["error"].concat(restrictedGlobals),
     // 'no-restricted-syntax': ['error', 'WithStatement'],
     // 'no-underscore-dangle': OFF,
-    'no-unused-expressions': OFF,
-    'no-use-before-define': OFF,
-    'prettier/prettier': [
-      'error',
+    // 'no-unused-expressions': OFF,
+    // 'no-use-before-define': OFF,
+    "prettier/prettier": [
+      ERROR,
       {
         singleQuote: true,
-        trailingComma: 'es5',
-      },
+        trailingComma: "es5"
+      }
     ],
-    'react/react-in-jsx-scope': OFF,
-    'import/no-extraneous-dependencies': [
-      'error',
-      { devDependencies: ['**/*.test.js', '**/__tests__/**'] },
-    ],
+    "react/react-in-jsx-scope": OFF,
+    // "filenames/match-exported": [2, [ null, "kebab", "snake" ] ],
+    "import/no-extraneous-dependencies": [
+      "error",
+      { devDependencies: ["**/*.stories.*", "**/*.test.*", "**/testing.*"] }
+    ]
     // 'react/prefer-stateless-function': OFF,
     // 'react/sort-comp': OFF,
     // 'react/destructuring-assignment': OFF,
-  },
+  }
 };
