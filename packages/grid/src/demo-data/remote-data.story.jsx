17d7b145319e261cb2a7ae0a4fec2d8cd0d85353
vHades <anhvu.nd.0199@gmail.com>
2019-09-19 13:39:44 +0700

    fix

---
+++ packages/grid/src/demo-data/remote-data.story.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+import { PagingState, SortingState, CustomPaging } from '@devexpress/dx-react-grid';
+import { Grid, Table, TableHeaderRow, PagingPanel } from '../index';
+
+import { Loading } from '../extensions/loading';
+import { CurrencyTypeProvider } from '../extensions/currency-type-provider';
+
+const URL = 'https://js.devexpress.com/Demos/WidgetsGallery/data/orderItems';
+
+export const RemoteData = () => {
+  const [columns] = useState([
+    { name: 'OrderNumber', title: 'Order #' },
+    { name: 'OrderDate', title: 'Order Date' },
+    { name: 'StoreCity', title: 'Store City' },
+    { name: 'Employee', title: 'Employee' },
+    { name: 'SaleAmount', title: 'Sale Amount' },
+  ]);
+  const [rows, setRows] = useState([]);
+  const [currencyColumns] = useState(['SaleAmount']);
+  const [tableColumnExtensions] = useState([
+    { columnName: 'OrderNumber', align: 'right' },
+    { columnName: 'SaleAmount', align: 'right' },
+  ]);
+  const [sorting, setSorting] = useState([{ columnName: 'StoreCity', direction: 'asc' }]);
+  const [totalCount, setTotalCount] = useState(0);
+  const [pageSize, setPageSize] = useState(10);
+  const [pageSizes] = useState([5, 10, 15]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [lastQuery, setLastQuery] = useState();
+
+  const changePageSize = value => {
+    const totalPages = Math.ceil(totalCount / value);
+    const updatedCurrentPage = Math.min(currentPage, totalPages - 1);
+
+    setPageSize(value);
+    setCurrentPage(updatedCurrentPage);
+  };
+
+  const getQueryString = () => {
+    let queryString = `${URL}?take=${pageSize}&skip=${pageSize * currentPage}`;
+
+    const columnSorting = sorting[0];
+    if (columnSorting) {
+      const sortingDirectionString = columnSorting.direction === 'desc' ? ' desc' : '';
+      queryString = `${queryString}&orderby=${columnSorting.columnName}${sortingDirectionString}`;
+    }
+
+    return queryString;
+  };
+
+  const loadData = () => {
+    const queryString = getQueryString();
+    if (queryString !== lastQuery && !loading) {
+      setLoading(true);
+      fetch(queryString)
+        .then(response => response.json())
+        .then(data => {
+          setRows(data.items);
+          setTotalCount(data.totalCount);
+          setLoading(false);
+        })
+        .catch(() => setLoading(false));
+      setLastQuery(queryString);
+    }
+  };
+
+  useEffect(() => loadData());
+
+  return (
+    <div className="card position-relative">
+      <Grid rows={rows} columns={columns}>
+        <CurrencyTypeProvider for={currencyColumns} />
+        <SortingState sorting={sorting} onSortingChange={setSorting} />
+        <PagingState
+          currentPage={currentPage}
+          onCurrentPageChange={setCurrentPage}
+          pageSize={pageSize}
+          onPageSizeChange={changePageSize}
+        />
+        <CustomPaging totalCount={totalCount} />
+        <Table columnExtensions={tableColumnExtensions} />
+        <TableHeaderRow showSortingControls />
+        <PagingPanel pageSizes={pageSizes} />
+      </Grid>
+      {loading && <Loading />}
+    </div>
+  );
+};
--- packages/grid/src/extensions/loading.jsx
+++ packages/grid/src/extensions/loading.jsx
@@ -1,10 +1,19 @@
-import * as React from 'react';
-import './loading.css';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background: rgba(255, 255, 255, 0.3);
+  height: 100%;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
+`;
 
 export const Loading = () => (
-  <div className="loading-shading">
-    <div className="loading-icon">
-      <span className="oi oi-loop-circular" />
-    </div>
-  </div>
+  <Wrapper className="d-flex justify-content-center align-items-center">
+    <FontAwesomeIcon icon={faSyncAlt} size="lg" spin />
+  </Wrapper>
 );
