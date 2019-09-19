124aab529752d3d8c6e74559d05b82ae556cf539
vHades <anhvu.nd.0199@gmail.com>
2019-09-19 23:43:42 +0700

    fix

--- packages/grid/src/demo-data/demos.stories.jsx
+++
@@ -1,4 +0,0 @@
-export * from './remote-data.story';
-export * from './virtual-grid.story';
-
-export default { title: 'Demos', parameters: { jest: ['group-panel-container'] } };
---
+++ packages/grid/stories/storybook/addons.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import '@storybook/addon-actions/register';
+import '@storybook/addon-cssresources/register';
+import '@storybook/addon-jest/register';
+import '@storybook/addon-storysource/register';
+import '@storybook/addon-viewport/register';
+import '@storybook/addon-events/register';
